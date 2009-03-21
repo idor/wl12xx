fe1a6ee96e811f65f05979c65a6cdd941f56125c
Luis R. Rodriguez <lrodriguez@atheros.com>
2009-03-20 23:46:11 -0400

    mac80211_hwsim: let the reg workqueue breathe when regtest is set
    Without this the regulatory domain isn't seen and we end up
    intersecting for each request (each radio).
    
    Signed-off-by: Luis R. Rodriguez <lrodriguez@atheros.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/mac80211_hwsim.c
+++ drivers/net/wireless/mac80211_hwsim.c
@@ -1035,6 +1035,9 @@ static int __init init_mac80211_hwsim(void)
 			break;
 		}
 
+		/* give the regulatory workqueue a chance to run */
+		if (regtest)
+			schedule_timeout_interruptible(1);
 		err = ieee80211_register_hw(hw);
 		if (err < 0) {
 			printk(KERN_DEBUG "mac80211_hwsim: "
