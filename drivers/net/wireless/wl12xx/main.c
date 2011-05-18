e343066c77833097b16ce1a4ec2259964d65285b
Arik Nemtsov <arik@wizery.com>
2011-05-18 06:30:03 +0300

    wl12xx: configure correct hlid in Rx BA sessions
    Set the correct hlid when sending the Rx BA session initiation command
    to the FW.

--- drivers/net/wireless/wl12xx/main.c
+++ drivers/net/wireless/wl12xx/main.c
@@ -3557,6 +3557,7 @@ static int wl1271_op_ampdu_action(struct ieee80211_hw *hw,
 {
 	struct wl1271 *wl = hw->priv;
 	int ret;
+	u8 hlid;
 
 	wl1271_debug(DEBUG_MAC80211, "mac80211 ampdu action %d tid %d", action,
 		     tid);
@@ -3569,6 +3570,18 @@ static int wl1271_op_ampdu_action(struct ieee80211_hw *hw,
 
 	if (unlikely(wl->state == WL1271_STATE_OFF)) {
 		ret = -EAGAIN;
+		goto out;
+	}
+
+	if (wl->bss_type == BSS_TYPE_STA_BSS) {
+		hlid = wl->sta_hlid;
+	} else if (wl->bss_type == BSS_TYPE_AP_BSS) {
+		struct wl1271_station *wl_sta;
+
+		wl_sta = (struct wl1271_station *)sta->drv_priv;
+		hlid = wl_sta->hlid;
+	} else {
+		ret = -EINVAL;
 		goto out;
 	}
 
@@ -3596,7 +3609,8 @@ static int wl1271_op_ampdu_action(struct ieee80211_hw *hw,
 			break;
 		}
 
-		ret = wl1271_acx_set_ba_receiver_session(wl, tid, *ssn, true);
+		ret = wl1271_acx_set_ba_receiver_session(wl, tid, *ssn, true,
+							 hlid);
 		if (!ret) {
 			wl->ba_rx_bitmap |= BIT(tid);
 			wl->ba_rx_session_count++;
@@ -3611,7 +3625,8 @@ static int wl1271_op_ampdu_action(struct ieee80211_hw *hw,
 			break;
 		}
 
-		ret = wl1271_acx_set_ba_receiver_session(wl, tid, 0, false);
+		ret = wl1271_acx_set_ba_receiver_session(wl, tid, 0, false,
+							 hlid);
 		if (!ret) {
 			wl->ba_rx_bitmap &= ~BIT(tid);
 			wl->ba_rx_session_count--;
